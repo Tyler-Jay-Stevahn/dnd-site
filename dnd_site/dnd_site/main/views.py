34f18c177d424773b2b7532a8f7e3b97f8fdd63e
Tyler-Jay-Stevahn <tylerjay1996@hotmail.com>
2020-03-21 13:06:47 -0500

    fixed redundant folder

--- dnd_site/dnd_site/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
-def landing_page(request):
-    context = {
-        "greeting":"heylo"
-    }
-    return render(request, 'landingpage.html', context)
--- dnd_site/main/views.py
+++ dnd_site/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def landing_page(request):
+    context = {
+        "greeting":"heylo"
+    }
+    return render(request, 'landingpage.html', context)
